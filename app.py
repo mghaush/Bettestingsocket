307fec9c0a0c38f58a2789639fa2de9e06649352
mghaush <112123430+mghaush@users.noreply.github.com>
2024-04-30 18:38:20 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-import websockets
 import asyncio
-import os
- 
-# Creating WebSocket server
+import websockets
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+# Create a list to store all connected WebSocket clients
 connected_clients = []
 
 # WebSocket server
@@ -21,13 +23,27 @@ async def ws_handler(websocket, path):
         connected_clients.remove(websocket)
         print(f"Client disconnected, total clients: {len(connected_clients)}")
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
- 
-async def main():
-    async with websockets.serve(ws_handler, "bettingsocket-d89de658d946.herokuapp.com"):
-        print(f"PORT SET")
-        await asyncio.Future()  # run forever
- 
-if __name__ == "__main__":
-    print(f"PORT SET")
-    asyncio.run(main())
+@app.route('/broadcast', methods=['POST'])
+def broadcast_message():
+    #message = request.form.get('message')
+    message = "Hi"
+    if message:
+        # Broadcast the message to all connected WebSocket clients
+        for client in connected_clients:
+            asyncio.create_task(client.send(message))
+        return 'Message broadcasted successfully'
+    else:
+        return 'No message provided', 400
+
+# Start the WebSocket server
+start_server = websockets.serve(ws_handler, "bettingsocket-d89de658d946.herokuapp.com")
+
+if __name__ == '__main__':
+    #app.run(host='localhost', port=443, debug=True, threaded=True)
+    # Start the WebSocket server in a separate event loop
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
